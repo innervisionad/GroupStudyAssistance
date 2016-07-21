38a634ab8ec050ca7131ee7e157491579b8cd6b2
innervisionad <aj.clements@live.co.uk>
2016-07-21 11:57:25 +0100

    overwatchDatabase

---
+++ overwatchDatabase/overwatchDatabase/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using overwatchDatabase.Models;
+using System.Net;
+
+namespace overwatchDatabase.Controllers
+{
+    public class HomeController : Controller
+    {
+        private overBaseEntities db = new overBaseEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var heros = from h in db.
+                        select h;
+            return View(heros);
+        }
+    }
+}
